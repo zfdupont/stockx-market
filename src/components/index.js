480562d36fea073745afefc7485bf083441cd4d3
zfdupont <zachary.dupont@stonybrook.edu>
2021-07-21 14:32:42 -0400

    multipage components and navbar

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Navigation } from "./Navigation";
+export { default as Account } from "./Account";
+export { default as WatchLists } from "./WatchLists";
+export { default as Orders } from "./Orders";
+export { default as Home } from "./Home";
