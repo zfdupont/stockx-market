ee393035fa0d78dffa95b1fb7b6b0691840f56c7
StevenJin7820 <steven.jin@stonybrook.edu>
2021-07-21 14:57:35 -0400

    Publishing branch

---
+++ src/scraper.js
@@ -0,0 +1,14 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+
+async function getPriceSneaker() {
+    try{
+        const siteUrl = 'stockx.com/'
+
+        const data 
+
+    } catch(err) {
+        console.log(err)
+    }
+}
