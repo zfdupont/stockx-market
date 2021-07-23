70ccd158ac462710f2ad56d6beb751308ad1537f
MichaelLa99 <NuroKnight@gmail.com>
2021-07-22 20:22:44 -0400

    Added ui elements

--- src/components/WatchLists.jsx
+++ src/components/WatchLists.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-
+import './WatchList.css';
 function WatchLists() {
     return (
         <div className="lists">
             <div class="container">
                 <div>
                     <h1>Watch Lists</h1>
-                    <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting
-                        industry. Lorem Ipsum has been the industry's standard dummy text
-                        ever since the 1500s, when an unknown printer took a galley of
-                        type and scrambled it to make a type specimen book.
-                    </p>
+                        <form id = "to-do-form">
+                            <p id = "shoes"> Your Watchlists</p>
+                            <input type = "text" placeholder = "Enter Shoe Name/SKU"/>
+                            <button type = "Submit">Add</button>
+                        </form>
+                        <div id = "view-lists">
+                        <select>
+                            <option value = ""> View 1 </option>
+                            <option value = ""> View 2 </option>
+                            <option value = ""> View 3 </option>
+                        </select>
+                    </div>
                 </div>
             </div>
         </div>
