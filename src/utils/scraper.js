d4672ce75fc14560dc01a80a57cc296c5e51e2c6
StevenJin7820 <steven.jin@stonybrook.edu>
2021-07-22 20:20:13 -0400

    FIltered

--- src/utils/scraper.js
+++ src/utils/scraper.js
@@ -18,14 +18,13 @@ async function getPriceSneaker(name) {
          const info = await got(siteUrl)
          const data = info.body.toString();
 
-        // Constant used by cheerio to find the data we want from the scraped axios data
-        const Elem = '#root > div.wrapper.css-1bwwf5m-AppContainer.e1ms8ude0 > div.page-container.css-4qyi6t-BannerPaddingWrapper > div.chakra-container.new-product-view.css-vp2g1e > section:nth-child(3) > div.css-gg4vpm > div.css-0 > div.buy-sell-container.css-qt7qal > div.chakra-stack.buy-sell-buttons-desktop.css-12vwdz3 > div:nth-child(1) > div > dl > dd'
         // Cheerio loads this data into a readable format
         const $ = cheerio.load(data)
         // Using the priceSelector and sellSelector constant cheerio finds all price values on the page and stores that into the object
-        console.log('Price: ', $('dd').text())
-        console.log('Size: ', $('dt').text())
-
+        console.log('Price:', $('dd').text())
+        var price = $('dd').text();
+        console.log('Size:', $('dt').text())
+        var size = $('dt').text();
         // Completed shoe object is saved into dictionary
         shoes[name] = shoe;
 
