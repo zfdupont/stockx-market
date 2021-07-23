8dcdc56c58966830485f596036add9fc326a0916
StevenJin7820 <steven.jin@stonybrook.edu>
2021-07-22 20:04:59 -0400

    Working but needs to be filtered

--- src/utils/scraper.js
+++ src/utils/scraper.js
@@ -3,7 +3,6 @@ const got = require('got');
 
 async function getPriceSneaker(name) {
     try{
-
         //Shoe object that holds name, buy price, and sell price
         const shoe = new Object();
         shoe.name = name;
@@ -23,9 +22,9 @@ async function getPriceSneaker(name) {
         const Elem = '#root > div.wrapper.css-1bwwf5m-AppContainer.e1ms8ude0 > div.page-container.css-4qyi6t-BannerPaddingWrapper > div.chakra-container.new-product-view.css-vp2g1e > section:nth-child(3) > div.css-gg4vpm > div.css-0 > div.buy-sell-container.css-qt7qal > div.chakra-stack.buy-sell-buttons-desktop.css-12vwdz3 > div:nth-child(1) > div > dl > dd'
         // Cheerio loads this data into a readable format
         const $ = cheerio.load(data)
-
         // Using the priceSelector and sellSelector constant cheerio finds all price values on the page and stores that into the object
-        console.log('Price: ', $(Elem))
+        console.log('Price: ', $('dd').text())
+        console.log('Size: ', $('dt').text())
 
         // Completed shoe object is saved into dictionary
         shoes[name] = shoe;
@@ -38,4 +37,4 @@ async function getPriceSneaker(name) {
     }
 }
 
-getPriceSneaker('air-jordan-1-retro-high-electro-orange')
+getPriceSneaker('nike-sb-dunk-low-ftc-lagoon-pulse')
