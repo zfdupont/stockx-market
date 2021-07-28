4bf0e5c10eae631b3bf8e088db459371f48563eb
StevenJin7820 <steven.jin@stonybrook.edu>
2021-07-28 19:19:56 -0400

    Login/Register page not rendering

---
+++ client/src/components/Register.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import "./style.css"
+const user = require("../models/Users");
+
+function createUser(e) {
+    e.preventDefault()
+    const {FirstName, LastName, Username, Email, Password} = e.target.elements
+    console.log(e.target.elements)
+    var user = new user({
+        firstName : FirstName.value,
+        lastName : LastName.value,
+        username : Username.value,
+        email : Email.value,
+        password : Password.value
+    })
+    user.save();
+}
+
+function Register(){
+    return(
+        <div>
+            <form onSubmit = {createUser}>
+            <h3>Register</h3>
+
+            <div className="form-group">
+                <label>First name</label>
+                <input type="text" className="form-control" placeholder="First name" />
+            </div>
+
+            <div className="form-group">
+                <label>Last name</label>
+                <input type="text" className="form-control" placeholder="Last name" />
+            </div>
+
+            <div className="form-group">
+                <label>Username</label>
+                <input type="text" className="form-control" placeholder="Username" />
+            </div>
+
+            <div className="form-group">
+                <label>Email</label>
+                <input type="email" className="form-control" placeholder="Enter email" />
+            </div>
+
+            <div className="form-group">
+                <label>Password</label>
+                <input type="password" className="form-control" placeholder="Enter password" />
+            </div>
+
+            <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
+            <p className="forgot-password text-right">
+                Already registered <a href="#">log in?</a>
+            </p>
+        </form>
+    </div>
+    )
+}
+
+export default Register;
