971f45ec07096177e750c3d3a6e3baf81a8d2a65
StevenJin7820 <steven.jin@stonybrook.edu>
2021-08-04 15:19:54 -0400

    Pulled package.json from backend

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -18,42 +18,44 @@ function createUser(e) {
 
 function Register(){
     return(
-        <div>
-            <form onSubmit = {createUser}>
-            <h3>Register</h3>
+        <div className = "register">
+            <div className = "container">
+                <form onSubmit = {createUser}>
+                <h3>Register</h3>
 
-            <div className="form-group">
-                <label>First name</label>
-                <input type="text" className="form-control" placeholder="First name" />
-            </div>
+                <div className="form-group">
+                    <label>First name</label>
+                    <input type="text" className="form-control" placeholder="First name" />
+                </div>
 
-            <div className="form-group">
-                <label>Last name</label>
-                <input type="text" className="form-control" placeholder="Last name" />
-            </div>
+                <div className="form-group">
+                    <label>Last name</label>
+                    <input type="text" className="form-control" placeholder="Last name" />
+                </div>
 
-            <div className="form-group">
-                <label>Username</label>
-                <input type="text" className="form-control" placeholder="Username" />
-            </div>
+                <div className="form-group">
+                    <label>Username</label>
+                    <input type="text" className="form-control" placeholder="Username" />
+                </div>
 
-            <div className="form-group">
-                <label>Email</label>
-                <input type="email" className="form-control" placeholder="Enter email" />
-            </div>
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
 
-            <div className="form-group">
-                <label>Password</label>
-                <input type="password" className="form-control" placeholder="Enter password" />
-            </div>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
 
-            <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
-            <p className="forgot-password text-right">
-                Already registered <a href="#">log in?</a>
-            </p>
-        </form>
+                <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
+                <p className="forgot-password text-right">
+                    Already registered <a href="#">log in?</a>
+                </p>
+            </form>
+        </div>
     </div>
-    )
+    );
 }
 
 export default Register;
